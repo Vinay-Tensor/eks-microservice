c5310f2780c9ec02e38506ac3c4edaaf3bbd7c4a
Vijay Nallagatla <vijay.nallagatla@iceye.fi>
2025-05-30 17:32:09 +0530

    add the iam role and security group

---
+++ terraform-modules/aws/eks/security_groups.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "eks_cluster" {
+  name        = "eks-cluster-sg"
+  description = "EKS cluster security group"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-cluster-sg"
+  }
+}
